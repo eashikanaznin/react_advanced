662ea53701c7639b40450c9015e0c285887665c5
Eashika Naznin <eashikanaznin@Eashikas-MacBook-Pro.local>
2024-06-10 12:24:11 +0600

    lazy and suspence

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { Suspense, lazy } from "react"
+
+const delay = (ms) =>
+  new Promise((resolve) => setTimeout(resolve, ms)); // Creates a promise that resolves after ms milliseconds
+
+
+// import { MyCustomData } from "./MyCustomData"
+
+const MyCustomData = lazy(() => delay(5000).then(()=>import( "./MyCustomData" )))
 function App() {
 
   return (
     <>
       hi
+      <Suspense fallback={<div>Loading Custom Data ....</div>} >
+        <MyCustomData />
+      </Suspense>
     </>
   )
 }
---
+++ src/MyCustomData.jsx
@@ -0,0 +1,9 @@
+export default function MyCustomData() {
+  return (
+    <div>
+      <h2>My Custom Data</h2>
+      <p>This is some custom data loaded after a delay.</p>
+    </div>
+  );
+};
+
