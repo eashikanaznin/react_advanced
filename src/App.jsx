42df8bf1ca023488b099676a7c03201c2d065959
eashika <eashika07@gmail.com>
2024-07-31 01:48:09 +0600

    fixed basic main

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,7 @@
-import { Child } from "./Child";
-import { useState } from "react";
 function App() {
-  const [enableDog, setEnableDog] = useState(true);
-  function toggleAnimal() {
-    setEnableDog((n) => !n);
-  }
   return (
-    <>
-      {enableDog ? (
-        <>
-          <span>For Dog</span>
-          <Child key="dog" />
-        </>
-      ) : (
-        <>
-          <span>For Cat</span>
-          <Child key="cat" />
-        </>
-      )}
-      {/*  With a common input. */}
-      {enableDog ? (
-        <>
-          <span>For Dog</span>
-        </>
-      ) : (
-        <>
-          <span>For Cat</span>
-        </>
-      )}
-      <input type="text" key={enableDog ? "dog" : "cat"} />
-      <button onClick={toggleAnimal}>Toggle animal</button>
-    </>
-  );
+   <h1>Hi</h1> 
+  )
 }
 
 export default App;
