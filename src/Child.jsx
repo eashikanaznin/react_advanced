5472822ca63d8380583775e273491be56f8ad8b3
eashika <eashika07@gmail.com>
2024-07-31 01:41:33 +0600

    advance key

--- src/Child.jsx
+++ src/Child.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 
 export function Child() {
   const [age, setAge] = useState(0);
-
   return (
     <div>
       <button onClick={() => setAge((a) => a - 1)}>-</button>
