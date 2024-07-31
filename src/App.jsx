4d6346ef66ef0e60670e052a2707b446e871e004
eashika <eashika07@gmail.com>
2024-07-31 11:19:48 +0600

    capture phese

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-function App() {
+import React from "react";
+
+const App = () => {
+  const handleCaptureClick = (event) => {
+    console.log("Capture phase - App");
+  };
+
+  const handleBubbleClick = (event) => {
+    console.log("Bubble phase - App");
+  };
+
   return (
-   <h1>Hi</h1> 
-  )
-}
+    <div
+      onClickCapture={handleCaptureClick} // Capture phase
+     // onClick={handleCaptureClick} // without capture
+       // Bubble phase
+    >
+      <span onClick={handleBubbleClick} >TTTT</span>
+      <button onClick={() => console.log("Button clicked")}>Click Me</button>
+    </div>
+  );
+};
 
 export default App;
