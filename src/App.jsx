d91e93e1aeaffad11b2dacd77d1aa99fa4f9d72c
eashika <eashika07@gmail.com>
2024-07-31 14:34:06 +0600

    useImparetiveHandle

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import { useRef } from "react";
-import { CustomRefCom } from "./CustomRefCom";
-function App() {
-  const myRef = useRef();
+import { useRef } from "react"
+import { CustomInput } from "./CustomInput"
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    alert(myRef.current.value);
+
+function App() {
+
+  // const inputRef = useRef()
+  const inputRef = useRef()
+
+  function handleSubmit(e){
+    e.preventDefault()
+    // alert(inputRef.current.value)
+    alert(inputRef.current.value)
   }
-  return (
-    <form onSubmit={handleSubmit}>
-      {/* <CustomRefCom ref={myRef} /> */}
-      <button>Submit</button>
-    </form>
-  );
+return (
+  <form onSubmit={ handleSubmit} >
+    {/* <input ref={ inputRef } placeholder ="Enter data" /> */}
+    <CustomInput ref={ inputRef } placeholder ="Enter data" />
+    <button type='submit'>submit</button>
+  </form>
+)
 }
 
-export default App;
+export default App
