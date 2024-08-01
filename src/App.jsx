452c1adf8b38f266281b5eb12740df9bd5604c9b
eashika <eashika07@gmail.com>
2024-08-01 17:19:50 +0600

    LDP-215 kyles excercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-function App() {
+import { useRef, useState } from "react"
+import "./styles.css"
+import { useToast } from "./useToast"
+
+export default function App() {
+  const { addToast, removeToast } = useToast()
+  const inputRef = useRef(null)
+  const [id, setId] = useState()
+
+  function createToast() {
+    if (inputRef.current == null || inputRef.current.value === "") return
+
+    setId(addToast(inputRef.current.value, { position: "top-left" }))
+  }
+
   return (
-   <h1>Hi</h1> 
+    <div className="form">
+      <input type="text" ref={inputRef} />
+      <button onClick={createToast}>Add Toast</button>
+      <button onClick={() => id != null && removeToast(id)}>
+        Remove Last Toast
+      </button>
+    </div>
   )
 }
-
-export default App;
